64eef615a393c6c365a660903b02d7694e43c15c
Some random studyshort dev <95879019+AStudyShortDev@users.noreply.github.com>
2021-12-13 20:39:37 +0100

    This scraper uses selenium instead
    Instead of BeautifulSoup, this scraper uses Selenium. At the moment this is just an example but it clicks on the "Login" hyperlink by finding the text. I'll add a way to choose reliable sources by using the text finder.

--- scraper.py
+++ scraper.py
@@ -1,25 +1,10 @@
-from os import system, name
-from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import by
+from selenium.webdriver.chrome.service import Service
 import csv
+import time
 
-def clear():
-    if name == 'nt':
-        _ = system('cls')
-    else:
-        _ = system('clear')
+page_to_scrape = wevdriver.Chrome(service=browser_driver)
+page_to_scrape.get("https://quotes.toscrape.com/").text
 
-page_to_scrape = requests.get("https://quotes.toscrape.com/").text
-soup = BeautifulSoup(page_to_scrape, "html.parser")
-quotes = soup.findAll("span", attrs={"class":"text"})
-authors = soup.findAll("small", attrs={"class":"author"})
-
-file = open("scraped_quotes.csv", "w")
-writer = csv.writer(file)
-writer.writerow(["QUOTES", "AUTHORS"])
-
-for quote, author in zip(quotes, authors):
-  print(quote.text + " - " + author.text)
-  print("\n")
-  writer.writerow([quote.text + " - " + author.text])
-file.close()
+page_to_scrape.find_element(By.LINK_TEXT, "Login").click()
